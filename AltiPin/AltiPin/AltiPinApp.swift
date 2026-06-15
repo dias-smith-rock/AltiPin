e32f0cc2f698b1f75d8ab1a6850d89c819d1a0f1
rock.gaox <rock.gaox@gmail.com>
2026-06-15 12:42:14 +0800

    init project

---
+++ AltiPin/AltiPin/AltiPinApp.swift
@@ -0,0 +1,32 @@
+//
+//  AltiPinApp.swift
+//  AltiPin
+//
+//  Created by Rock on 15/6/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AltiPinApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
